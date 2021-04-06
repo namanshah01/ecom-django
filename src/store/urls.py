310ff1492de0e35251ffdf38926dff7cc1b47120
Naman Shah <nashah2001@gmail.com>
2021-04-07 05:14:42 +0530

    add to cart logic done

--- src/store/urls.py
+++ src/store/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import home, cart, checkout
+from .views import *
 
 urlpatterns = [
 	path('home/', home, name='home'),
 	path('cart/', cart, name='cart'),
 	path('checkout/', checkout, name='checkout'),
+	path('update_item/', updateItem, name='update_item'),
 ]
--- src/store/views.py
+++ src/store/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import *
+from django.http import JsonResponse
+import json
 # Create your views here.
 
 def home(request):
@@ -28,3 +30,26 @@ def checkout(request):
 		order	= {'get_cart_quantity': 0, 'get_cart_total': 0}
 	context	= {'items': items, 'order': order}
 	return render(request, 'store/checkout.html', context)
+
+def updateItem(request):
+	data		= json.loads(request.body)
+	productId	= data['productId']
+	action		= data['action']
+	print(f'productId: {productId}\naction: {action}')
+
+	account				= request.user.account
+	product				= Product.objects.get(id=productId)
+	order, created		= Order.objects.get_or_create(account=account)
+	orderItem, created	= OrderItem.objects.get_or_create(order=order, product=product)
+
+	if action == 'add':
+		orderItem.quantity += 1
+	elif action == 'remove':
+		orderItem.quantity -= 1
+	
+	orderItem.save()
+
+	if orderItem.quantity <= 0:
+		orderItem.delete()
+	
+	return JsonResponse('Item added', safe=False)
