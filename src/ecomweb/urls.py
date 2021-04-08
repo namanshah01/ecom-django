00d3367001e2a03085f85795fb09e3d0b5fe536c
Naman Shah <nashah2001@gmail.com>
2021-04-08 18:06:06 +0530

    guess it worked

--- src/ecomweb/urls.py
+++ src/ecomweb/urls.py
@@ -41,35 +41,35 @@ urlpatterns = [
 	path('account/', account_view, name="account"),
 
 	# Password reset links (ref: https://github.com/django/django/blob/master/django/contrib/auth/views.py)
-	# path('password_change/done/',
-	# 	auth_views.PasswordChangeDoneView.as_view(template_name='registration/password_change_done.html'),
-	# 	name='password_change_done'
-	# ),
+	path('password_change/done/',
+		auth_views.PasswordChangeDoneView.as_view(template_name='users/password_change_done.html'),
+		name='password_change_done'
+	),
 
-	# path('password_change/',
-	# 	auth_views.PasswordChangeView.as_view(template_name='registration/password_change.html'),
-	# 	name='password_change'
-	# ),
+	path('password_change/',
+		auth_views.PasswordChangeView.as_view(template_name='users/password_change.html'),
+		name='password_change'
+	),
 
-	# path('password_reset/done/',
-	# 	auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'),
-	# 	name='password_reset_done'
-	# ),
+	path('password_reset/done/',
+		auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_done.html'),
+		name='password_reset_done'
+	),
 
-	# path('reset/<uidb64>/<token>/',
-	# 	auth_views.PasswordResetConfirmView.as_view(),
-	# 	name='password_reset_confirm'
-	# ),
+	path('reset/<uidb64>/<token>/',
+		auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
+		name='password_reset_confirm'
+	),
 
-	# path('password_reset/',
-	# 	auth_views.PasswordResetView.as_view(),
-	# 	name='password_reset'
-	# ),
+	path('password_reset/',
+		auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'),
+		name='password_reset'
+	),
 	
-	# path('reset/done/',
-	# 	auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'),
-	# 	name='password_reset_complete'
-	# ),
+	path('reset/done/',
+		auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'),
+		name='password_reset_complete'
+	),
 ]
 
 if settings.DEBUG:
